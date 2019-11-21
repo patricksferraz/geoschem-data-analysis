f8858482307c8511d2c75416720f6ac5550a364c
patricksferraz <patrick.ferraz@outlook.com>
2019-11-20 23:08:33 -0300

    feat(analysis): adds with and without cartography

--- analysis.py
+++ analysis.py
@@ -22,7 +22,8 @@ ARGS = {
     "trac_nc": "temp/geos45s/trac_avg.geosfp_4x5_standard.201501010000.nc",
     "tracerinfo": "temp/geos45s/tracerinfo.dat",
     "diaginfo": "temp/geos45s/diaginfo.dat",
-    "f_out": lambda x: f"out/img/{x}_emission.png",
+    "fw_out": lambda x, l: f"out/img/{x}_cartopy_lev_{l}.png",
+    "f_out": lambda x, l: f"out/img/{x}_lev_{l}.png",
 }
 
 # %%
@@ -93,9 +94,24 @@ times = [t.values.astype("datetime64[M]") for t in emissions["time"]]
 
 # %%
 
+# Plot without cartography
 for emission in emissions.values():
     fig, axes = plt.subplots(
-        1, 2, figsize=(15, 5), subplot_kw={"projection": ccrs.PlateCarree()}
+        1, 2, figsize=(10, 4), subplot_kw={"projection": ccrs.PlateCarree()}
+    )
+
+    # Plot BPCH diagnotics for each time
+    for ax, t in zip(axes, rlen(times)):
+        emission.isel(time=t, lev=0).plot(ax=ax, cmap=WhGrYlRd)
+
+    plt.savefig(ARGS["f_out"](emission.attrs["name"], 0))
+
+# %%
+
+# Plot with cartography
+for emission in emissions.values():
+    fig, axes = plt.subplots(
+        1, 2, figsize=(10, 4), subplot_kw={"projection": ccrs.PlateCarree()}
     )
 
     # Plot BPCH diagnotics for each time
@@ -103,11 +119,11 @@ for emission in emissions.values():
         emission.isel(time=t, lev=0).plot(
             ax=ax, cmap=WhGrYlRd, cbar_kwargs={"shrink": 0.5, "label": "ppbv"}
         )
-        ax.set_title(f"{emission.attrs['name']} concentration - {times[t]}")
+        ax.set_title(f"{emission.attrs['name']} lev 0 - {times[t]}")
         ax.coastlines()
         ax.gridlines(linestyle="--")
 
-    plt.savefig(ARGS["f_out"](emission.attrs["name"]))
+    plt.savefig(ARGS["fw_out"](emission.attrs["name"], 0))
 
 
 # %%
